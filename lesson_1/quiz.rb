c96483abf901f329e5bd9871dd90a282f9bda898
Kramer Keller <kramerkeller@gmail.com>
2018-02-26 10:14:10 -0500

    Initialize git

---
+++ lesson_1/quiz.rb
@@ -0,0 +1,64 @@
+# Quiz 1
+
+# I think they will all be yes, but lets test so I get the answer right
+# Good thing I tested... I was wrong...
+
+string = "To be or not to be"
+
+#1
+
+def string_lengths(sentence)
+  strings = sentence.split
+
+  strings.map { |chars| chars.length }
+end
+
+p string_lengths(string)
+
+#2
+
+def string_lengths(sentence)
+  strings = sentence.split
+  lengths = []
+
+  strings.each do |string|
+    lengths << string.size
+  end
+end
+
+p string_lengths(string)
+
+#3
+
+def string_lengths(sentence)
+  words = sentence.split
+  word_lengths = []
+  counter = 0
+
+  while counter < words.size do
+    word_lengths << words[counter].length
+    counter += 1
+  end
+
+  word_lengths
+end
+
+p string_lengths(string)
+
+#4
+
+def string_lengths(sentence)
+  strings = sentence.split
+  lengths = []
+  counter = 1
+
+  until counter == strings.size do
+    word_length = strings[counter - 1].length
+    lengths.push(word_length)
+    counter += 1
+  end
+
+  lengths
+end
+
+p string_lengths(string)
