328cbbbac520365b2af36e53261a5b8fa1d6c19f
Kramer Keller <kramerkeller@gmail.com>
2018-03-16 16:19:40 -0400

    TTT Bonus Features 3

--- 06_lesson/05_tictactoe.rb
+++ 06_lesson/05_tictactoe.rb
@@ -74,8 +74,25 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def find_at_risk_square(line, board)
+  if board.values_at(*line).count(PLAYER_MARKER) == 2
+    board.select{|k,v| line.include?(k) && v == ' '}.keys.first
+  else
+    nil
+  end
+end
+
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd)
+    break if square
+  end
+
+  if !square
+    square = empty_squares(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
